7ea5a84353eace93f7064e59e61544675158df30
Sean Bollin <sean@sean-bollin.com>
2017-03-05 15:53:23 -0800

    run npm test with xvfb

--- src/Integration.test.js
+++ src/Integration.test.js
@@ -3,9 +3,9 @@ import nightmare from 'nightmare'
 it('should receive a server response', (done) => {
 	nightmare()
 	.goto('http://0.0.0.0:8000')
-	.wait(5000)
+	.wait(500)
 	.click('button.send')
-	.wait(5000)
+	.wait(500)
 	.evaluate(() => document.querySelector('ul li').innerText)
 	.end()
 	.then((v) => { expect(v).toBe("Hello from obsidian-server!"); done(); })
